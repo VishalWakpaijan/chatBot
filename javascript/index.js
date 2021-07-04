c852f82626645b702779b7b33664a0620e01d683
Vishal wakpaijan <VW56500@gmail.com>
2021-07-04 17:26:39 +0530

    added functionalty

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,14 @@
 const chatBox = document.getElementById("chatBot");
 const chatBotButton = document.getElementById("chatBotButton");
+const heading = document.getElementById("heading");
+
+chatBotButton.addEventListener("click", getChatBox);
 
 function getChatBox() {
-  chatBox.classList.toggle("chatBot_box_active");
+  const clicked = chatBox.classList.toggle("chatBot_box_active");
+  if (clicked === true) {
+    heading.style.display = "none";
+  } else {
+    heading.style.display = "block";
+  }
 }
-chatBotButton.addEventListener("click", getChatBox);
